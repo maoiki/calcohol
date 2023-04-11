31cfab92e9b18e633ed394a5fb014400893414ac
Julio Duarte <julio.franca.017@ufrn.edu.br>
2023-04-10 22:41:36 -0300

    #3 feat: add database connection prototype

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,23 +1,108 @@
 <script>
-import useAuthUser from 'src/composables/UseAuthUser';
-import { defineComponent} from 'vue'
+import useAuthUser from "src/composables/UseAuthUser";
+import { defineComponent, ref, onMounted } from "vue";
+import useApi from "src/composables/UseApi";
+import useNotify from "src/composables/UseNotify";
 
 export default defineComponent({
-  name: 'MePage',
-  setup(){
-    const {user} = useAuthUser()
-    return{
-      user
-    }
-  }
+  name: "MePage",
+  setup() {
+    const loading = ref(true);
+
+    const { user } = useAuthUser();
+
+    const { list } = useApi();
+
+    const { notifyError } = useNotify();
+
+    const beverages = ref([]);
+
+    const handleListBeverages = async () => {
+      try {
+        loading.value = true;
+        beverages.value = await list("beverages");
+        loading.value = false;
+      } catch (error) {
+        notifyError(error.message);
+      }
+    };
+
+    onMounted(() => {
+      handleListBeverages();
+    });
+
+    const columns = [
+      {
+        name: "name",
+        label: "name",
+        align: "left",
+        field: "name",
+        sortable: true,
+      },
+      {
+        name: "actions",
+        align: "right",
+        label: "actions",
+        field: "actions",
+        sortable: true,
+      },
+    ];
+
+    return {
+      user,
+      beverages,
+      columns,
+      loading,
+    };
+  },
 });
 </script>
 
 <template>
-  <q-page class="flex flex-center">
-      <h1>Hi, {{ user.user_metadata.name }}!</h1>
-      <p>{{ user }}</p>
+  <q-page padding>
+    <h1>Hi, {{ user.user_metadata.name }}!</h1>
+    <div class="col-12">
+      <q-table
+        title=" @@@ TITULO @@@"
+        :rows="beverages"
+        :columns="columns"
+        row-key="name"
+        :loading="loading"
+      >
+      <template v-slot:top>
+          <span class="text-h6">
+            Titulo
+          </span>
+          <q-space />
+          <q-btn
+            v-if="$q.platform.is.desktop"
+            label="Add New"
+            color="primary"
+            icon="add"
+            dense
+          />
+        </template>
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props" class="q-gutter-x-sm">
+            <q-btn
+              icon="edit"
+              color="info"
+              dense
+              size="sm"
+            >
+              <q-tooltip> Edit </q-tooltip>
+            </q-btn>
+            <q-btn
+              icon="delete"
+              color="negative"
+              dense
+              size="sm"
+            >
+              <q-tooltip> Delete </q-tooltip>
+            </q-btn>
+          </q-td>
+        </template>
+      </q-table>
+    </div>
   </q-page>
 </template>
-
-
